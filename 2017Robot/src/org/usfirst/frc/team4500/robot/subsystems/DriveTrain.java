aa925d665589e46f1da86b2ff7ec446f02eb530a
Nicolas Newman <nnewman316@gmail.com>
2017-01-27 18:14:44 -0600

    Changed the OmniDrive function code

--- 2017Robot/src/org/usfirst/frc/team4500/robot/subsystems/DriveTrain.java
+++ 2017Robot/src/org/usfirst/frc/team4500/robot/subsystems/DriveTrain.java
@@ -39,42 +39,53 @@ public class DriveTrain extends Subsystem {
 //	}
     
     public void omniDrive(double joyX, double joyY, double joyTwist) {
-    	if(joyY > 0.1) {
-    		SmartDashboard.putString("In Y+", "Yes");
-    		lOmni.set(0.5);
-    		rOmni.set(0.5);
-    	} else if(joyY < -0.1) {
-    		SmartDashboard.putString("In Y-", "Yes");
-    		lOmni.set(-joyY);
-    		rOmni.set(-0.5);
-    	} else {
-    		lOmni.set(0);
-    		rOmni.set(0);
-    	}
+		fsOmni.set(joyX);
+		bsOmni.set(joyX);
+		lOmni.set(joyY);
+		rOmni.set(joyY);
+//    	if(joyTwist == 0) {
+//    		lOmni.set(0);
+//    		rOmni.set(0);
+//    	}
+//    	if(joyY > 0.1) {
+//    		SmartDashboard.putString("In Y+", "Yes");
+//    		lOmni.set(joyY);
+//    		rOmni.set(joyY);
+//    	} else if(joyY < -0.1) {
+//    		SmartDashboard.putString("In Y-", "Yes");
+//    		lOmni.set(joyY);
+//    		rOmni.set(joyY);
+//    	} else {
+//    		lOmni.set(0);
+//    		rOmni.set(0);
+//    	}
+//    	
+//    	if(joyX > 0.1) {
+//    		SmartDashboard.putString("In X+", "Yes");
+//    		fsOmni.set(joyX);
+//    		bsOmni.set(joyX);
+//    	} else if(joyX < -0.1) {
+//    		SmartDashboard.putString("In X-", "Yes");
+//    		fsOmni.set(joyX);
+//    		bsOmni.set(joyX);
+//    	} else {
+//    		fsOmni.set(0);
+//    		bsOmni.set(0);
+//    	}
+//    	
+//    	if(joyTwist > 0.1) {
+//    		lOmni.set(joyTwist);
+//    		rOmni.set(joyTwist);
+//    	} else if(joyTwist < -0.1) {
+//    		lOmni.set(joyTwist);
+//    		rOmni.set(joyTwist);
+//    	} else {
+//    		lOmni.set(joyTwist);
+//    		rOmni.set(joyTwist);
+//    	}
     	
-    	if(joyX > 0.1) {
-    		SmartDashboard.putString("In X+", "Yes");
-    		fsOmni.set(0.5);
-    		bsOmni.set(0.5);
-    	} else if(joyX < -0.1) {
-    		SmartDashboard.putString("In X-", "Yes");
-    		fsOmni.set(-0.5);
-    		bsOmni.set(-0.5);
-    	} else {
-    		fsOmni.set(0);
-    		bsOmni.set(0);
-    	}
     	
-    	if(joyTwist > 0.1) {
-    		lOmni.set(-0.5);
-    		rOmni.set(0.5);
-    	} else if(joyTwist < -0.1) {
-    		lOmni.set(0.5);
-    		rOmni.set(-0.5);
-    	} else {
-    		lOmni.set(0);
-    		rOmni.set(0);
-    	} 	
+    	
     }
 }
 
